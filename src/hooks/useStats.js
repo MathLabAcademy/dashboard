6bb27f401b02b8e9bd5a49ef970112e8a44197c8
MunifTanjim <muniftanjim@gmail.com>
2020-01-08 00:14:15 +0600

    Add DailyTransactionsForYearStats UI

---
+++ src/hooks/useStats.js
@@ -0,0 +1,26 @@
+import { stringify } from 'query-string'
+import { useEffect, useMemo, useState } from 'react'
+import api from 'utils/api'
+import { emptyObject } from 'utils/defaults'
+
+export function useStats(key, queryObject = emptyObject) {
+  const query = useMemo(() => stringify(queryObject), [queryObject])
+
+  const [data, setData] = useState()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    api(`/stats/${key}?${query}`)
+      .then(({ data }) => {
+        setData(data)
+        setLoading(false)
+      })
+      .catch(err => {
+        console.error(err)
+        setLoading(false)
+      })
+  }, [key, query])
+
+  return [data, loading]
+}
--- src/pages/index/Main.js
+++ src/pages/index/Main.js
@@ -1,8 +1,78 @@
 import { get } from 'lodash-es'
 import { connect } from 'react-redux'
+import { ResponsiveCalendar } from '@nivo/calendar'
+import React, { useCallback, useRef, useState } from 'react'
+import { Box } from 'reflexbox'
+import { useStats } from 'hooks/useStats'
+import { Card, Input, Button, Header } from 'semantic-ui-react'
+import HeaderGrid from 'components/HeaderGrid'
+
+function DailyTransactionsForYearStats() {
+  const yearRef = useRef()
+
+  const [year, setYear] = useState(new Date().getFullYear())
+
+  const handleYearChange = useCallback(() => {
+    if (!yearRef.current) return
+    const year = yearRef.current.inputRef.current.value
+    setYear(Number(year))
+  }, [])
+
+  const [data = [], loading] = useStats('daily-transactions-for-year', { year })
+
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Card.Header>
+          <HeaderGrid
+            Left={<Header>Transaction Stats for {year}</Header>}
+            Right={
+              <Input
+                ref={yearRef}
+                defaultValue={year}
+                type="number"
+                min="2000"
+                max="2099"
+                step="1"
+                icon="calendar alternate"
+                iconPosition="left"
+                action={
+                  <Button
+                    loading={loading}
+                    type="button"
+                    icon="refresh"
+                    onClick={handleYearChange}
+                  />
+                }
+              />
+            }
+          />
+        </Card.Header>
+        <Card.Description>
+          <Box height={200}>
+            <ResponsiveCalendar
+              data={data}
+              from={`${year}-01-01`}
+              to={`${year}-12-31`}
+              emptyColor="#eee"
+              margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+              monthBorderColor="#fff"
+              dayBorderWidth={2}
+              dayBorderColor="#fff"
+            />
+          </Box>
+        </Card.Description>
+      </Card.Content>
+    </Card>
+  )
+}
 
 function DashIndex() {
-  return null
+  return (
+    <Box>
+      <DailyTransactionsForYearStats />
+    </Box>
+  )
 }
 
 const mapStateToProps = ({ user }) => ({
@@ -11,7 +81,4 @@ const mapStateToProps = ({ user }) => ({
 
 const mapDispatchToProps = {}
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(DashIndex)
+export default connect(mapStateToProps, mapDispatchToProps)(DashIndex)
