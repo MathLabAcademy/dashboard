3bf5c5a7157a7b9f2ee5803f234fb894737e9e2f
Munif Tanjim <hello@muniftanjim.dev>
2023-01-28 23:02:26 +0600

    Add summary for student mcq exam result view

--- src/components/Course/MCQExams/Result.js
+++ src/components/Course/MCQExams/Result.js
@@ -1,10 +1,12 @@
+import { Stack, Text } from '@chakra-ui/core'
 import { DraftViewer } from 'components/Draft'
 import HeaderGrid from 'components/HeaderGrid'
 import Permit from 'components/Permit'
 import { get, keyBy, mapValues } from 'lodash-es'
+import { DateTime } from 'luxon'
 import React, { useEffect, useMemo, useState } from 'react'
 import { connect } from 'react-redux'
-import { Grid, Header, Segment, Icon } from 'semantic-ui-react'
+import { Grid, Header, Icon, Segment } from 'semantic-ui-react'
 import {
   getAllQuestionsForExam,
   getAllSubmissions,
@@ -52,8 +54,60 @@ function MCQExamResult({
     )
   }, [data])
 
+  const summary = useMemo(() => {
+    const correctOptionIdByQuestionId = mapValues(
+      keyBy(get(data, 'Questions', emptyArray), 'id'),
+      'Answer.id'
+    )
+
+    return {
+      answersSubmitted: get(data, 'Submissions', emptyArray).length,
+      correctlyAnswered: get(data, 'Submissions', emptyArray).filter(
+        ({ mcqId, mcqOptionId }) => {
+          return mcqOptionId === correctOptionIdByQuestionId[mcqId]
+        }
+      ).length,
+      totalQuestions: get(data, 'Questions', emptyArray).length,
+      startTime: DateTime.fromISO(
+        get(data, 'Trackers[0].start')
+      ).toLocaleString(DateTime.DATETIME_MED),
+      endTime: DateTime.fromISO(get(data, 'Trackers[0].end')).toLocaleString(
+        DateTime.DATETIME_MED
+      ),
+      lastPingTime: DateTime.fromISO(
+        get(data, 'Trackers[0].ping')
+      ).toLocaleString(DateTime.DATETIME_MED),
+    }
+  }, [data])
+
   return (
     <Permit roles="student">
+      <Segment>
+        <Stack>
+          <Text>
+            <strong>Exam Start Time: </strong>
+            {summary.startTime}
+          </Text>
+          <Text>
+            <strong>Last Ping Time: </strong>
+            {summary.lastPingTime}
+          </Text>
+          <Text>
+            <strong>Exam End Time: </strong>
+            {summary.endTime}
+          </Text>
+          <br />
+          <Text>
+            <strong>Answers Submitted: </strong>
+            {summary.answersSubmitted} / {summary.totalQuestions}
+          </Text>
+          <Text>
+            <strong>Correctly Answered: </strong>
+            {summary.correctlyAnswered} / {summary.totalQuestions}
+          </Text>
+        </Stack>
+      </Segment>
+
       {get(data, 'Questions', emptyArray).map((question, index) => (
         <Segment key={get(question, 'id')}>
           <HeaderGrid
