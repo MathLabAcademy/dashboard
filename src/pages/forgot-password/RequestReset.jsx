d7baaac428d99561b726870bef759b6d3fb4b34c
Munif Tanjim <hello@muniftanjim.dev>
2024-01-15 21:46:00 +0600

    Fix password reset request page

--- src/pages/forgot-password/RequestReset.jsx
+++ src/pages/forgot-password/RequestReset.jsx
@@ -1,17 +1,11 @@
-import { Link } from 'react-router-dom'
+import { Box, Stack } from '@chakra-ui/core'
 import Form from 'components/Form/Form'
 import FormInput from 'components/Form/Input'
 import { Formik } from 'formik'
 import queryString from 'query-string'
 import React, { useCallback, useMemo, useState } from 'react'
-import {
-  Button,
-  Container,
-  FormField,
-  FormGroup,
-  Message,
-  Segment,
-} from 'semantic-ui-react'
+import { Link, useLocation } from 'react-router-dom'
+import { Button, FormField, FormGroup, Grid, Message } from 'semantic-ui-react'
 import { trackEventAnalytics } from 'utils/analytics'
 import api from 'utils/api'
 import * as Yup from 'yup'
@@ -25,7 +19,9 @@ const getInitialValues = (email) => ({
   email: email || '',
 })
 
-function RequestPasswordReset({ location }) {
+function RequestPasswordReset() {
+  const location = useLocation()
+
   const [sent, setSent] = useState(false)
 
   const email = useMemo(() => {
@@ -63,67 +59,78 @@ function RequestPasswordReset({ location }) {
   }, [])
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      isInitialValid={Boolean(email)}
-      onSubmit={onSubmit}
-    >
-      {({ isSubmitting, isValid, values, status }) => (
-        <Form>
-          <Container text textAlign="center" as={Segment}>
-            <Message color="yellow" hidden={!status}>
-              {status}
-            </Message>
+    <Grid columns={1} centered padded>
+      <Grid.Column mobile={16} tablet={12} style={{ maxWidth: '720px' }}>
+        <Box borderWidth={1} p={6}>
+          <Stack spacing={2} textAlign="center">
+            <Box>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                isInitialValid={Boolean(email)}
+                onSubmit={onSubmit}
+              >
+                {({ isSubmitting, isValid, values, status }) => (
+                  <Form>
+                    <Box>
+                      <Message color="yellow" hidden={!status}>
+                        {status}
+                      </Message>
 
-            {sent ? (
-              <Message>
-                <p>
-                  If an account exists for <strong>{values.email}</strong>, an
-                  email will be sent with further instructions to reset your
-                  password.
-                </p>
-                <p>
-                  <em>Didn't receive the email?</em>
-                  <br />
-                  <em>
-                    Wait a few minutes and make sure you've checked your spam
-                    folder!
-                  </em>
-                </p>
-              </Message>
-            ) : (
-              <FormInput
-                name="email"
-                type="email"
-                label={`Enter your email address`}
-                placeholder={`Email`}
-                icon="envelope"
-              />
-            )}
+                      {sent ? (
+                        <Message>
+                          <p>
+                            If an account exists for{' '}
+                            <strong>{values.email}</strong>, an email will be
+                            sent with further instructions to reset your
+                            password.
+                          </p>
+                          <p>
+                            <em>Didn't receive the email?</em>
+                            <br />
+                            <em>
+                              Wait a few minutes and make sure you've checked
+                              your spam folder!
+                            </em>
+                          </p>
+                        </Message>
+                      ) : (
+                        <FormInput
+                          name="email"
+                          type="email"
+                          label={`Enter your email address`}
+                          placeholder={`Email`}
+                          icon="envelope"
+                        />
+                      )}
 
-            <FormGroup widths="equal">
-              <FormField width="6">
-                <Button fluid as={Link} to="/login">
-                  Go Back
-                </Button>
-              </FormField>
-              <FormField width="10">
-                <Button
-                  positive
-                  fluid
-                  type="submit"
-                  loading={isSubmitting}
-                  disabled={!isValid || isSubmitting || sent}
-                >
-                  Request Reset
-                </Button>
-              </FormField>
-            </FormGroup>
-          </Container>
-        </Form>
-      )}
-    </Formik>
+                      <FormGroup widths="equal">
+                        <FormField width="6">
+                          <Button fluid as={Link} to="/login">
+                            Go Back
+                          </Button>
+                        </FormField>
+                        <FormField width="10">
+                          <Button
+                            positive
+                            fluid
+                            type="submit"
+                            loading={isSubmitting}
+                            disabled={!isValid || isSubmitting || sent}
+                          >
+                            Request Reset
+                          </Button>
+                        </FormField>
+                      </FormGroup>
+                    </Box>
+                  </Form>
+                )}
+              </Formik>
+            </Box>
+          </Stack>
+        </Box>
+      </Grid.Column>
+    </Grid>
   )
 }
 
